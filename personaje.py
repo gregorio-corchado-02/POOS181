4a7478fb58ee76b3846dadf2527c4d1621784f31
gregorioooC <125932869+gregorioooC@users.noreply.github.com>
2023-02-20 18:44:00 -0600

    Definimos atributos de los personajes

--- personaje.py
+++ personaje.py
@@ -6,3 +6,16 @@ class Personaje:
     altura = 1.78
     
     #Metodos del personaje
+    def correr(self,status):
+        if(status):
+            print("El personaje" + self.nombre+"esta corriendo")
+        else:
+            print("El personaje" + self.nombre+"se detuvo")
+            
+    def lanzarGranada(self):
+        print("Se lanzo granada")
+        
+    def recargarArma (self, municiones):
+        cargador = 5
+        cargador = cargador + municiones
+        print("El arma tiene: "+ cargador+"balas")
