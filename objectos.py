e6780b5edd14302fdfb357cafcc1ee696e475d94
gregorioooC <125932869+gregorioooC@users.noreply.github.com>
2023-02-24 18:26:32 -0600

    Encapsulamiento

--- objectos.py
+++ objectos.py
@@ -1,45 +1,37 @@
-#1.- Importar la clase
 from personaje import *
 
-#2.- Solicitar atributos del objecto
 print("")
-print("Solicitar de datos heroe")
-espH= input("Escribe la especie")
-nomH= input("Escribe el nombre")
-altH= input("Escribe la altura")
-cargaH= input("Cuntas balas recarga el heroe")
+print("Solicitar de datos heroe ")
+espH= input("Escribe la especie ")
+nomH= input("Escribe el nombre ")
+altH= input("Escribe la altura ")
+cargaH=int( input("Cuntas balas recarga el heroe "))
 
 print("")
 print("Solicitar de datos villano")
-espV= input("Escribe la villano")
-nomV= input("Escribe el villano")
-altV= input("Escribe la villano")
-cargaV= input("Cuntas balas recarga el villano")
+espV= input("Escribe la epecie villano ")
+nomV= input("Escribe el nombre villano ")
+altV= input("Escribe la altura villano ")
+cargaV= int(input("Cuntas balas recarga el villano "))
 
 Heroe = Personaje(espH, nomH, altH)
 Villano= Personaje(espV, nomV, altV)
 
-#3.- Accerder a sus atributos
+Heroe.setNombre("Pepe pecas")
 
-print("El personaje pertenece a la raza:"+ Heroe.especie)
-print("El personaje se llama:"+ Heroe.nombre)
-print("El personaje mide:"+ str(Heroe.altura) + "Metros")
+print("El personaje pertenece a la raza: "+ Heroe.getEspecie())
+print("El personaje se llama: "+ Heroe.getNombre())
+print("El personaje mide: "+ str(Heroe.getAltura()) + "Metros")
 Heroe.correr(True)
 Heroe.lanzarGranada()
-Heroe.recargarArma(68)
+Heroe.recargarArma(cargaH)
+#Heroe.__pensar()
 
-Heroe.correr(True)
-Heroe.lanzarGranada()
-Heroe.recargarArma
 
-print("El personaje pertenece a la raza:"+ Heroe.especie)
-print("El personaje se llama:"+ Heroe.nombre)
-print("El personaje mide:"+ str(Heroe.altura) + "Metros")
-
-print("El personaje pertenece a la raza:"+ Villano.especie)
-print("El personaje se llama:"+ Villano.nombre)
-print("El personaje mide:"+ str(Villano.altura) + "Metros")
+print("El personaje pertenece a la raza: "+ Villano.getEspecie())
+print("El personaje se llama: "+ Villano.getNombre())
+print("El personaje mide: "+ str(Villano.getAltura()) + "Metros")
 
 Villano.correr(True)
 Villano.lanzarGranada()
-Villano.recargarArma
+Villano.recargarArma(cargaV)
--- personaje.py
+++ personaje.py
@@ -1,17 +1,15 @@
 class Personaje:
     
-    #Creamos al personaje
     def __init__(self,esp,nom,alt):
-        self.especie = esp
-        self.nombre = nom
-        self.altura = alt
+        self.__especie = esp
+        self.__nombre = nom
+        self.__altura = alt
 
-    #Metodos del personaje
     def correr(self,status):
         if(status):
-            print("El personaje" + self.nombre+"esta corriendo")
+            print("El personaje" + self.__nombre+"esta corriendo")
         else:
-            print("El personaje" + self.nombre+"se detuvo")
+            print("El personaje" + self.__nombre+"se detuvo")
             
     def lanzarGranada(self):
         print("Se lanzo granada")
@@ -20,3 +18,24 @@ class Personaje:
         cargador = 5
         cargador = cargador + municiones
         print("El arma tiene: "+ str(cargador) +"balas")
+        
+    def __pensar(self):
+        print("esta pensando")
+        
+    def getEspecie(self):
+        return self.__especie
+    
+    def setEspecie(self,esp):
+        self.__especie=esp
+        
+    def getNombre(self):
+        return self.__nombre
+    
+    def setNombre(self,nom):
+        self.__nombre=nom
+        
+    def getAltura(self):
+        return self.__altura
+    
+    def setAltura(self,alt):
+        self.__altura=alt
